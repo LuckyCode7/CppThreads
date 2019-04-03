4947bc494c1e77907b9e63cea726219a2d8ed8c5
Rafał Nowak <nowak9.private@gmail.com>
2019-04-03 23:29:05 +0200

    mutex added

--- std_accumulate/accumulate.h
+++ std_accumulate/accumulate.h
@@ -1,53 +1,58 @@
 #pragma once
+#include <mutex>
 #include <math.h>
 #include <thread>
+#include <vector>
 
-std::mutex m_;
-
-template<class InputIt, class T>
-T accumulate(InputIt first, InputIt last, T init)
+namespace my_accumulate
 {
-    const unsigned int threads_counter = thread::hardware_concurrency() != 0 ? thread::hardware_concurrency() : 2;
-    const unsigned int elements_counter = std::distance(first, last);
+    std::mutex m_;
 
-    if (!elements_counter) return init;
-
-    bool hardWork = false; // last thread has to sum up more numbers then the rest of threads 
-    std::vector<thread> sums;
-    unsigned int numbersToAdd = 0;
-    unsigned int numbersToAddCounter = 0;
-
-    if (elements_counter % threads_counter == 0)
+    template<class InputIt, class T>
+    T accumulate(InputIt first, InputIt last, T init)
     {
-        numbersToAdd = elements_counter / threads_counter;
-    }
-    else
-    {
-        numbersToAdd = floor(static_cast<double>(elements_counter) / static_cast<double>(threads_counter));
-        hardWork = true;
-    }
+        const unsigned int threads_counter = thread::hardware_concurrency() != 0 ? thread::hardware_concurrency() : 2;
+        const unsigned int elements_counter = std::distance(first, last);
 
-    for (int i = 0; i < threads_counter; ++i)
-    {
-        sums.emplace_back([&] 
+        if (!elements_counter) return init;
+
+        bool hardWork = false; // last thread has to sum up more numbers then the rest of threads 
+        std::vector<thread> sums;
+        unsigned int numbersToAdd = 0;
+        unsigned int numbersToAddCounter = 0;
+
+        if (elements_counter % threads_counter == 0)
         {
-            for (; first != last; ++first)
+            numbersToAdd = elements_counter / threads_counter;
+        }
+        else
+        {
+            numbersToAdd = floor(static_cast<double>(elements_counter) / static_cast<double>(threads_counter));
+            hardWork = true;
+        }
+
+        for (int i = 0; i < threads_counter; ++i)
+        {
+            sums.emplace_back([&]
             {
-                if((numbersToAddCounter == numbersToAdd) && !(i = threads_counter - 1 && hardWork))
-                {   
-                    numbersToAddCounter = 0;
-                    break;
+                for (; first != last; ++first)
+                {
+                    if ((numbersToAddCounter == numbersToAdd) && !(i = threads_counter - 1 && hardWork))
+                    {
+                        numbersToAddCounter = 0;
+                        break;
+                    }
+                    m_.lock();
+                    init = std::move(init) + *first;
+                    m_.unlock();
+                    numbersToAddCounter++;
                 }
-                m_.lock();
-                init = std::move(init) + *first;
-                m_.unlock();
-                numbersToAddCounter++;
-            }
-        });
+            });
+        }
+
+        for (auto& x : sums)
+            x.join();
+
+        return init;
     }
-
-    for (auto& x : sums)
-        x.join();
-
-    return init;
 }
