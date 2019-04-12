3717ca008e051c13b5aa1629ea0daa4065653af1
LuckyCode7 <nowak9.private@gmail.com>
2019-04-12 14:32:02 +0200

    solution finished

--- feasting_philosophers/feastingPhilosophers.h
+++ feasting_philosophers/feastingPhilosophers.h
@@ -2,75 +2,90 @@
 #include <thread>
 #include <mutex>
 #include <vector>
+#include <future>
 #include <iostream>
 #include <random>
+#include <atomic>
 using namespace std;
 
-class Fork
+struct Fork
 {
-  public:
     unsigned int ID;
     mutex fork_mutex;
 };
 
-class Philosoph
+struct Philosoph
 {
-  public:
     unsigned int ID;
     Fork first_fork;
     Fork second_fork;
-    void setForks(Fork &f1, Fork &f2)
+
+    void eat(Fork &f1, Fork &f2)
     {
         scoped_lock lock(f1.fork_mutex, f2.fork_mutex);
         first_fork.ID = f1.ID;
         second_fork.ID = f2.ID;
-        cout << "Philosoph [" << ID << "]\n";
-        cout << "\tFork[" << first_fork.ID << "]\n";
-        cout << "\tFork[" << second_fork.ID << "]\n";
+    }
+
+    friend ostream &operator<<(ostream &os, const Philosoph &p)
+    {
+        os << "Philosoph [" << p.ID << "]\n";
+        os << "\tFork[" << p.first_fork.ID << "]\n";
+        os << "\tFork[" << p.second_fork.ID << "]";
+        return os;
     }
 };
 
-void solution()
+void setPhilosophsAndForks(vector<Philosoph> &philosophs, vector<Fork> &forks)
 {
-    vector<Philosoph> Philosophs(5);
-    vector<Fork> Forks(5);
+    for (unsigned int i = 0; i < philosophs.size(); ++i)
+    {
+        philosophs[i].ID = i;
+        forks[i].ID = i;
+    }
+}
+
+void showPhilosophs(vector<Philosoph> &philosophs)
+{
+    for (const auto &x : philosophs)
+    {
+        cout << x << endl;
+    }
+}
+
+void feed_philosoph(vector<Philosoph> &philosophs, vector<Fork> &forks)
+{
     vector<thread> threads;
 
     std::random_device dev;
     std::mt19937 rng(dev());
     std::uniform_int_distribution<std::mt19937::result_type> num(1, 2);
 
-    for (int i = 0; i < 5; ++i)
-    {
-        Philosophs[i].ID = i;
-        Forks[i].ID = i;
-    }
-
-    auto feed_philosoph = [&](int i,  int randomFork) {
+    auto setter = [&](int randomFork, int i) {
         switch (randomFork)
         {
         case 1:
         {
             if (i == 0)
-                Philosophs[i].setForks(ref(Forks[i]), ref(Forks[Forks.size() - 1]));
+                philosophs[i].eat(ref(forks[i]), ref(forks[forks.size() - 1]));
             else
-                Philosophs[i].setForks(ref(Forks[i]), ref(Forks[i - 1]));
+                philosophs[i].eat(ref(forks[i]), ref(forks[i - 1]));
         }
         break;
         case 2:
         {
             if (i == 0)
-                Philosophs[i].setForks(ref(Forks[Forks.size() - 1]), ref(Forks[i]));
+                philosophs[i].eat(ref(forks[forks.size() - 1]), ref(forks[i]));
             else
-                Philosophs[i].setForks(ref(Forks[i - 1]), ref(Forks[i]));
+                philosophs[i].eat(ref(forks[i - 1]), ref(forks[i]));
         }
         break;
         }
     };
 
-    for (int i = 0; i < Philosophs.size(); ++i)
+    for (int i = 0; i < philosophs.size(); ++i)
     {
-        threads.emplace_back(feed_philosoph, i,num(rng));
+        threads.emplace_back(setter, num(rng), i);
     }
 
     for (auto &t : threads)
--- feasting_philosophers/main.cpp
+++ feasting_philosophers/main.cpp
@@ -1,8 +1,17 @@
 #include "feastingPhilosophers.h"
+#include <iostream>
+using namespace std;
 
-int main()
+int main(int argc, char *argv[])
 {
+    const int num_of_philosophs = atoi(argv[1]);
 
-    solution();
+    vector<Philosoph> philosophs(num_of_philosophs);
+    vector<Fork> forks(num_of_philosophs);
+
+    setPhilosophsAndForks(philosophs,forks);
+    feed_philosoph(philosophs,forks);
+    showPhilosophs(philosophs);
+
     return 0;
 }
